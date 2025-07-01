94407135772302816553a6711d0c4ae4745cfa0f
Nawaz Alam <137739784+jugnu141@users.noreply.github.com>
2025-07-01 17:17:00 +0530

    Update and rename 02_Coin_Change.cpp to 02_LEET-322_Coin_Change.cpp

--- 7.Recursion/02_LEET-322_Coin_Change.cpp
+++ 7.Recursion/02_LEET-322_Coin_Change.cpp
@@ -1,5 +1,40 @@
 //LEET-322 https://leetcode.com/problems/coin-change/
+//My Approach
+//Tutorial Approach
 
+//My Aprroach
+class Solution {
+public:
+    void solve(vector<int>& coins,vector<int>&ans,int i,int amount,long long int sum,int a)
+    {
+        //base case
+        if(sum==amount)
+        {
+            ans.push_back(a);
+            return;
+        }
+        if(i>=coins.size() || sum>=amount )
+            return;
+        //include
+        solve(coins,ans,i,amount,sum+coins[i],a+1);
+        //exclude
+        solve(coins,ans,i+1,amount,sum,a);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>ans;
+        int i=0;
+        long long int sum=0;
+        int a=0;
+        solve(coins,ans,i,amount,sum,a);
+        if(ans.empty())
+            return -1;
+        return *min_element(ans.begin(),ans.end());
+
+    }
+};
+
+//Tutorial Approach
 class Solution {
 public:
     int solve(vector<int>&coins,int amount)
