6641fe2d654205a03fae2dbbaa5743862b1c6100
Nawaz Alam <137739784+jugnu141@users.noreply.github.com>
2025-07-01 17:33:51 +0530

    Update 01_LEET-198_House_Robber.cpp

--- 7.Recursion/01_LEET-198_House_Robber.cpp
+++ 7.Recursion/01_LEET-198_House_Robber.cpp
@@ -1,5 +1,37 @@
 //LEET-198 https://leetcode.com/problems/house-robber/
+//Tutorial Approach
+//My Approach
 
+
+//My Approach
+class Solution {
+public:
+    void solve(vector<int>&nums,int i,int sum,int &maxSum)
+    {
+        //base case
+        if(i >= nums.size())
+        {
+            maxSum=max(maxSum,sum);
+            return;
+        }
+        //include
+        solve(nums,i+2,sum+nums[i],maxSum);
+        //exclude
+        solve(nums,i+1,sum,maxSum);
+    }
+    int rob(vector<int>& nums) {
+       int i=0;
+       //vector<int>money; storing in separate vector, causes memory limit 
+       int sum=0;
+       int maxSum=INT_MIN;
+       solve(nums,i,sum,maxSum);
+       //return *max_element(money.begin(),money.end());
+       return maxSum;
+    }
+};
+
+
+//Tutorial Approach
 class Solution {
 public:
     int solve(vector<int>&nums,int size,int i)
