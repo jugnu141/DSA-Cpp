92d566981b4389b1987dbf9a5a2be344e5c126cb
Nawaz Alam <137739784+jugnu141@users.noreply.github.com>
2025-07-03 18:53:07 +0530

    Create 08_LEET-121_Buy And Sell Stock.cpp

---
+++ 7.Recursion/Assignments/08_LEET-121_Buy And Sell Stock.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+public:
+    //By Recursion - in O(n) Time Complexity
+
+    void solve(vector<int>& prices,int &maxprofit,int &minprice,int i)
+    {
+        //Base Case
+        if(i>=prices.size())
+            return;
+
+        //1 case mai solve karunga,baaki recursion karega
+        // if(prices[i] < minprice)
+        //     minprice=prices[i];
+        minprice=min(minprice,prices[i]);
+
+        maxprofit=max(maxprofit,prices[i]-minprice);
+        solve(prices,maxprofit,minprice,i+1);
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int i=0;
+        int maxprofit=INT_MIN;
+        //int maxprice=INT_MIN;
+        int minprice=INT_MAX;
+        solve(prices,maxprofit,minprice,i);
+        if(maxprofit <= 0)
+            return 0;
+        else
+            return maxprofit;
+
+    }
+};
